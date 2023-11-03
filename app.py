67a3324b5d5dafaa797baaf39e7e5756b338d978
YashSunidhi <smnitrkl50@gmail.com>
2023-11-03 14:35:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -20,4 +20,5 @@ print("Loading Shapefile...")
 
 shapefile = gpd.read_file("./Data-for-CivTech-Exploration/ScottishForestry-TreeHealth-SurveySites-Oct23.shp")
 df = shapefile[(shapefile['pest_suspe']=='P. ramorum') & (shapefile['flag_sourc']=='Helicopter Survey') & (shapefile['host_speci']=='Larch') ].reset_index(drop=True)
-st.map(df.explore(column = 'lab_result'))
+df.explore(column = 'lab_result')
+st.pyplot()
