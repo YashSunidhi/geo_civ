ca8e3f0219c9a8b1acfef71d4e3dba952d6c0a36
YashSunidhi <smnitrkl50@gmail.com>
2023-11-03 13:33:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,21 +3,17 @@ import pandas as pd
 import datetime as datetime
 import os
 import ast
-from sentence_transformers import SentenceTransformer
-from bertopic.representation import KeyBERTInspired, MaximalMarginalRelevance, TextGeneration
 import plotly.express as px
+import geopandas as gpd
 
-from umap import UMAP
-from hdbscan import HDBSCAN
-from bertopic import BERTopic
-
-from sklearn.feature_extraction.text import CountVectorizer
-from bertopic.representation import PartOfSpeech, KeyBERTInspired, MaximalMarginalRelevance, OpenAI
-import streamlit as st
-from trubrics.integrations.streamlit import FeedbackCollector
-from streamlit_feedback import streamlit_feedback
-from streamlit_extras.stylable_container import stylable_container
 
 st.set_page_config(layout="wide")
 st.markdown("<h1 style='text-align: center; color: black;'> CivTech Data Analysis </h1>", unsafe_allow_html=True)
 st.markdown("<h5 style='text-align: center; color: black;'> Data Understansing </h5>", unsafe_allow_html=True)
+
+print("Loading Shapefile...")
+
+# If using your files, replace below filename ("Clean_Streets_Index_Grids_2018_Quarter_3.shp") with the 
+# shapefile filename you uploaded. 
+
+shapefile = gpd.read_file("/content/ScottishForestry-TreeHealth-SurveySites-Oct23.shp")
